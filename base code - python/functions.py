5ac5a8430eebb4a8d5a174ea5236dbe3ec519f2a
DMota03 <diogorsmota11@gmail.com>
2022-12-14 11:47:49 +0000

    Update functions.py
    o

--- base code - python/functions.py
+++ base code - python/functions.py
@@ -1,26 +1,22 @@
-from huffmantree import HuffmanTree
-
 def huffmanCode(lenghts):
-    #Slide 34 algorithm
-    temp = {}
     codes = {}
     bl_count = {}
     for i in range(max(lenghts.values())+1):
         bl_count[i] = 0
 
-    # Creates bl_cout and populates it
     for i in lenghts.keys():
 	    if(lenghts[i] != 0):
                 bl_count[lenghts[i]] += 1
                 bl_count[0] = 0
 
+    print("bl_count: ", bl_count)
     code = 0
     next_code = {}
     for i in range(1, max(lenghts.values())+1):
         code = (code + bl_count[i-1]) << 1
         next_code[i] = code
-    print(next_code)
-
+    print("next_code: ", next_code)
+    
     for i in bl_count.keys():
         if(bl_count[i] != 0):
             for j in lenghts.keys():
@@ -28,103 +24,5 @@ def huffmanCode(lenghts):
                     codes[j] = bin(next_code[i]).split('b')[1]
                     next_code[i] += 1
 
-    #Fills the binary code with zeros where it is suposed to be
-    for i in codes.keys():
-        if(len(codes[i]) < lenghts[i]):
-            codes[i] = (lenghts[i] - len(codes[i])) * "0" + codes[i]
-
-    for i in sorted(list(codes.keys())):
-        temp[i] = codes[i]
-    codes =temp
 
     return codes
-
-#Fucntion for exercice 4: Getting Hlit_Lens
-def search_tree_by_bit(gzip, HCLEN_tree, HLIT):
-    n = 0
-    HLIT_lenghts = {}
-    while n < HLIT:
-        bit = gzip.readBits(1)
-        pos = HCLEN_tree.nextNode(str(bit))
-        if(pos >= 0):
-            if(pos == 16):
-                repeat = 3
-                bit = 0
-                for i in range(2):
-                    bits = gzip.readBits(1)
-                    bits = bits << i
-                    bit = bit | bits
-                repeat += bit
-                for i in range(repeat):
-                    HLIT_lenghts[n] = HLIT_lenghts[n-1]
-                    n += 1
-            elif(pos == 17):
-                repeat = 3
-                bit = 0
-                for i in range(3):
-                    bits = gzip.readBits(1)
-                    bits = bits << i
-                    bit = bit | bits
-                repeat += bit
-                for i in range(repeat):
-                    HLIT_lenghts[n] = 0
-                    n+=1
-            elif(pos == 18):
-                repeat = 11
-                bit = 0
-                for i in range(7):
-                    bits = gzip.readBits(1)
-                    bits = bits << i
-                    bit = bit | bits
-                repeat += bit
-                for i in range(repeat):
-                    HLIT_lenghts[n] = 0
-                    n+=1
-            else:
-                HLIT_lenghts[n] = pos
-                n+=1
-            HCLEN_tree.resetCurNode()
-    return HLIT_lenghts
-
-def creates_tree(codes):
-    tree = HuffmanTree()
-    for i in codes.keys():tree.addNode(codes[i], i)
-    return tree
-
-def decompress(gzip, HLIT_tree, HDIST_tree):
-    output = []
-    pos = 0;
-    while pos != 256:
-        bit = gzip.readBits(1)
-        pos = HLIT_tree.nextNode(str(bit))
-        if(pos >= 0):
-            if(pos < 256):
-                output += [pos]
-            else:
-                size = [0,0]
-                if(256<pos<265):
-                    size[0] = pos-254
-                elif (pos == 285):
-                    size[0] = 258
-                else:
-                    #Algoritm to calculate bits to read
-                    toRead = ((pos-265)//4)+1
-                    aux = 0
-                    for i in range(toRead-1):
-                        if(toRead != i+1):
-                            aux += 2**(i+1)
-                    aux += (2**toRead)*((pos-264)%4) + 10
-                    size[0] = aux
-
-                dist = -2
-                while dist < 0:
-                    dist = HDIST_tree.nextNode(str(gzip.readBits(1)))
-                HDIST_tree.resetCurNode()
-                if (dist < 5):
-                    size[1] = dist+1
-                elif():
-
-                print(size)
-            HLIT_tree.resetCurNode()
-
-    return output
